04ece89c8abae7d7af505e147b3d45ab56d3d89d
Nathan Chevalier <cerberusgfx@gmail.com>
2023-11-02 13:21:10 -0500

    Updated bookCategory from DateField to DateTimeField

--- digestapi/models/bookCategory.py
+++ digestapi/models/bookCategory.py
@@ -3,4 +3,4 @@ from django.db import models
 class BookCategory(models.Model):
     book = models.ForeignKey("Book", on_delete=models.CASCADE, related_name="categories")
     category = models.ForeignKey("Category", on_delete=models.CASCADE, related_name="books")
-    date = models.DateField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
