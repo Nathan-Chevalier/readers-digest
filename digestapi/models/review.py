94f6f8ff94c8836d007f4b1c4f96c2b701e0d74e
Nathan Chevalier <cerberusgfx@gmail.com>
2023-11-02 13:14:22 -0500

    Reverted changes to review and category

--- digestapi/models/review.py
+++ digestapi/models/review.py
@@ -2,13 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
-def validate_rating_range(rating):
-    if not 1 <= rating <= 10:
-        raise ValidationError("Value must be between 1 and 10.") #? Conditional to set the review range from 1-5
+
 
 class Review(models.Model):
     book = models.ForeignKey("Book", on_delete=models.CASCADE, related_name="reviews")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
-    rating = models.PositiveIntegerField(max_length=2, validators=[validate_rating_range]) #? Specifies positive integer
+    rating = models.PositiveIntegerField() #? Specifies positive integer
     comment = models.CharField(max_length=2000)
     date = models.DateField(auto_now_add=True) #? auto_add_now sets the date upon creation and does not update
