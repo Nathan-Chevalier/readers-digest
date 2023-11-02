31c5ce254843af27de5d4c838f14f9c4f052c6c7
Nathan Chevalier <cerberusgfx@gmail.com>
2023-11-02 13:49:33 -0500

    Updated related_name to stop clash error in migration, migrated tables

--- digestapi/models/bookCategory.py
+++ digestapi/models/bookCategory.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class BookCategory(models.Model):
-    book = models.ForeignKey("Book", on_delete=models.CASCADE, related_name="categories")
-    category = models.ForeignKey("Category", on_delete=models.CASCADE, related_name="books")
+    book = models.ForeignKey("Book", on_delete=models.CASCADE, related_name="book_categories")
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, related_name="book_categories")
     date = models.DateTimeField(auto_now_add=True)
