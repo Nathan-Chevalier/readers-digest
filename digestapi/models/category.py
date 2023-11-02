b06d0c8d14fd1e3b0eb90383856a770cdc74b7e0
Nathan Chevalier <cerberusgfx@gmail.com>
2023-11-02 13:05:57 -0500

    Review updated for 10 scale, category updated with ManyToManyField

--- digestapi/models/category.py
+++ digestapi/models/category.py
@@ -2,3 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
+    books = models.ManyToManyField(
+        "Book",
+        through='BookCategory',
+        related_name="categories"
+    )
